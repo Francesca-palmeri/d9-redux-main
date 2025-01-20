9bdb9f0b777f449dd9f279003cc22386bae942c8
Francesca-palmeri <francesca.palmeri.c@gmail.com>
2025-01-20 17:01:14 +0100

    comm

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,14 @@
+const initialState = {
+  jobs: {
+    content: [],
+  },
+}
+
+const mainReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default mainReducer
